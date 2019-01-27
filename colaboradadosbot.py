f34dd52735bf1c227887096eca007e356b1a3623
Joao Ernane Barbosa <joaoernanepaula+gitlab@gmail.com>
2019-01-27 15:43:03 -0200

    Refatoração, criação do tweet_frases.py e outros ajustes menores.

--- colaboradadosbot.py
+++ colaboradadosbot.py
@@ -1,23 +1,16 @@
-#Importando as libraries
+# Importando as libraries
 
-import os
 import numpy as np
 import pandas as pd
-import requests
+from requests import get, exceptions
 import tweepy
-import configparser
 import settings
-import random
+from tweet_frases import tweet_arroba, tweet_orgao
 
-#Fingindo ser um humano
+# Fingindo ser um humano
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
-# Importando tokens
-
-config = configparser.ConfigParser()
-config.sections()
-config.read('colaboradados.conf')
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 # Autenticando o Twitter
 
@@ -32,50 +25,36 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 bot = tweepy.API(auth)
 
-#Abrindo a lista de portais da transparência
+# Abrindo a lista de portais da transparência
 
 df = pd.read_csv('lista_portais.csv', header=None, names=['url', 'arroba', 'orgao'], sep=';')
 
-#Mudando o NaN padrão do Pandas para ""
+# Mudando o NaN padrão do Pandas para ""
 
-df = df.replace(np.nan, '', regex=True) 
+df = df.replace(np.nan, '', regex=True)
 
-#Acessando urls
+# Acessando urls
 
 for index, row in df.iterrows():
-
-    try:
-        url = row['url']
-        arroba = row['arroba']
-        orgao = row['orgao']
-        status_code = requests.get(row['url'], headers=headers).status_code
-        
-#Lista de frases que o bot vai twittar
-
-        #Frases com nome do órgão
-
-        com_orgao = [f"O portal com dados públicos [ {url} ] do órgão {orgao} parece não estar funcionando. Poderia me ajudar a checar?",
-                     f"Hum, parece que o site [ {url} ], mantido pelo órgão {orgao}, está apresentando erro. Poderia dar uma olhadinha?",
-                     f"Poxa, tentei acessar [ {url} ] e não consegui. Este site é mantido pelo órgão {orgao}. Você pode confirmar isso?",
-                     f"Não consigo acessar [ {url} ], e eu sei que ele é mantido pelo órgão {orgao}. Você pode me ajudar a verificar?",
-                     f"Sabe o portal [ {url} ], mantido pelo orgão {orgao}? Ele parece estar fora do ar. Você pode confirmar?"]                    
-        msg_orgao = random.choice(com_orgao)
-        tweet_orgao = f'{msg_orgao}'
-
-        #Frases com arroba
-
-        com_arroba = [f"Parece que [ {url} ] está apresentando probleminhas para ser acessado. O que está acontecendo {arroba}?",
-                     f"Oi {arroba}, tudo bem? Pois com esse site [ {url} ] parece não estar, já que ele possui problemas de acesso.",
-                     f"Portais da transparência são um direito ao acesso à informação {arroba}, mas parece que [ {url} ] está fora do ar.",
-                     f"Opa {arroba}, parece que o site [ {url} ] não está acessível como deveria. O que está acontecendo?",
-                     f"Tentei acessar o site [ {url} ] e não consegui. {arroba} está acontecendo algum problema com essa portal de transparência?"]
-        msg_arroba = random.choice(com_arroba)
-        tweet_arroba = f'{msg_arroba}'
-
-#Dando diagnóstico e twittando
-
-    except (requests.HTTPError, requests.ConnectionError) as erro:
-        if arroba == "":
-            bot.update_status(tweet_orgao)
+    url = row['url']
+    arroba = row['arroba']
+    orgao = row['orgao']
+    for tentativas in range(10):
+        try:
+            status_code = get(row['url'], timeout=10).status_code
+            print(f'{url} : {status_code}\n{orgao}\n{arroba}\n')
+            if status_code != 200:
+                if arroba == "":
+                    print(tweet_orgao(url=url, orgao=orgao))
+                else:
+                    print(tweet_arroba(url=url, arroba=arroba))
+        except exceptions.ConnectionError:
+            continue
         else:
-            bot.update_status(tweet_arroba)
+            break
+    else:  # Caso exceda 10 tentativas de acessar o site.
+        if arroba == "":
+            print(tweet_orgao(url=url, orgao=orgao))
+        else:
+            print(tweet_arroba(url=url, arroba=arroba))
+        continue
---
+++ tweet_frases.py
@@ -0,0 +1,23 @@
+from random import choice
+
+
+def tweet_orgao(url, orgao):
+    com_orgao = [
+        f"O portal com dados públicos [ {url} ] do órgão {orgao} parece não estar funcionando. Poderia me ajudar a checar?",
+        f"Hum, parece que o site [ {url} ], mantido pelo órgão {orgao}, está apresentando erro. Poderia dar uma olhadinha?",
+        f"Poxa, tentei acessar [ {url} ] e não consegui. Este site é mantido pelo órgão {orgao}. Você pode confirmar isso?",
+        f"Não consigo acessar [ {url} ], e eu sei que ele é mantido pelo órgão {orgao}. Você pode me ajudar a verificar?",
+        f"Sabe o portal [ {url} ], mantido pelo orgão {orgao}? Ele parece estar fora do ar. Você pode confirmar?"]
+    msg_orgao = choice(com_orgao)
+    return msg_orgao
+
+
+def tweet_arroba(url, arroba):
+    com_arroba = [
+        f"Parece que [ {url} ] está apresentando probleminhas para ser acessado. O que está acontecendo {arroba}?",
+        f"Oi {arroba}, tudo bem? Pois com esse site [ {url} ] parece não estar, já que ele possui problemas de acesso.",
+        f"Portais da transparência são um direito ao acesso à informação {arroba}, mas parece que [ {url} ] está fora do ar.",
+        f"Opa {arroba}, parece que o site [ {url} ] não está acessível como deveria. O que está acontecendo?",
+        f"Tentei acessar o site [ {url} ] e não consegui. {arroba} está acontecendo algum problema com essa portal de transparência?"]
+    msg_arroba = choice(com_arroba)
+    return msg_arroba
