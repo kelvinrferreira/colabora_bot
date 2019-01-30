e7c52f3de0ba762c008393b7da3e167e233bea8f
Gilson Filho <me@gilsondev.in>
2019-01-30 15:51:37 -0200

    Melhorias no código para legibilidade e manutenção
    Essas melhorias foram feitas para facilitar a legibilidade e melhorar
    a manutenção, separando cada ação em uma função independente.
    
    Além disso foi removido os chamados valores mágicos, atribuindo-os a
    constantes que são auto explicativos.

--- colaboradadosbot.py
+++ colaboradadosbot.py
@@ -2,10 +2,14 @@
 
 import numpy as np
 import pandas as pd
-from requests import get, exceptions
 import tweepy
 import settings
+
+from requests import get, exceptions
 from tweet_frases import tweet_arroba, tweet_orgao
+
+TOTAL_RETRY = 10
+STATUS_SUCCESS = 200
 
 # Fingindo ser um humano
 
@@ -25,35 +29,56 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 bot = tweepy.API(auth)
 
-# Abrindo a lista de portais da transparência
 
-df = pd.read_csv('lista_portais.csv', header=None, names=['url', 'arroba', 'orgao'], sep=';')
+def tweet_status_site(arroba, orgao, url):
+    """Criando o tweet com o status do site recém acessado"""
+    if arroba == "":
+        bot.update_status(tweet_orgao(url=url, orgao=orgao))
+    else:
+        bot.update_status(tweet_arroba(url=url, arroba=arroba))
 
-# Mudando o NaN padrão do Pandas para ""
 
-df = df.replace(np.nan, '', regex=True)
+def load_site_data():
+    """
+    Abrindo a lista de portais da transparência e tratando
+    informações que serão tratados como NaN para o pandas.
+    """
+    df = pd.read_csv(
+        'lista_portais.csv',
+        header=None,
+        names=['url', 'arroba', 'orgao'],
+        sep=';'
+    )
+    df = df.replace(np.nan, '', regex=True)
 
-# Acessando urls
+    return df
 
-for index, row in df.iterrows():
-    url = row['url']
-    arroba = row['arroba']
-    orgao = row['orgao']
-    for tentativas in range(10):
-        try:
-            status_code = get(row['url'], timeout=10, headers=headers).status_code
-            if status_code != 200:
-                if arroba == "":
-                    bot.update_status(tweet_orgao(url=url, orgao=orgao))
-                else:
-                    bot.update_status(tweet_arroba(url=url, arroba=arroba))
-        except exceptions.ConnectionError:
-            continue
-        else:
-            break
-    else:  # Caso exceda 10 tentativas de acessar o site.
-        if arroba == "":
-            bot.update_status(tweet_orgao(url=url, orgao=orgao))
-        else:
-            bot.update_status(tweet_arroba(url=url, arroba=arroba))
-        continue
+
+def fetch_and_access_sites(sites):
+    """
+    Percorrendo a lista de sites para verificar
+    a sua disponibilidade. Caso o código de status
+    seja 200 (OK), então ela está disponível para acesso.
+
+    Caso contrário, envie um novo tweet avisando da indisponibilidade
+    citando o perfil do órgão no Twitter caso tenha.
+    """
+    for index, row in sites.iterrows():
+        url, arroba, orgao = row['url'], row['arroba'], row['orgao']
+
+        for tentativa in range(TOTAL_RETRY):
+            try:
+                response = get(row['url'], timeout=30, headers=headers)
+                if response.status_code == STATUS_SUCCESS:
+                    continue
+
+                tweet_status_site(arroba, orgao, url)
+            except exceptions.ConnectionError:
+                print(f"Problemas na conexão ao acessar o órgão {orgao}")
+            except exceptions.ReadTimeout:
+                tweet_status_site(arroba, orgao, url)
+
+
+if __name__ == '__main__':
+    sites = load_site_data()
+    fetch_and_access_sites(sites)
