5337b7863fbeb95f1a787469708ab9683bfe17ac
Joao Ernane Barbosa <joaoernanepaula+gitlab@gmail.com>
2019-05-06 16:12:15 -0300

    Atualizando colaborabot para versão 2.0
    Funções implementadas:
    
    + Bugfixes. (Não checa o mesmo site multiplas vezes arriscando bloqueio.)
    + Adicionada conta no Mastodon (instância bots in space).
    + Índice de tabelas criadas no Google Sheets.
    + Melhoria na organização e comentários.
    - Tweets suspensos no momento.

---
+++ autenticadores.py
@@ -0,0 +1,64 @@
+from authlib.client import AssertionSession
+from mastodon import Mastodon
+from credenciais import settings
+import tweepy
+import json
+
+
+def masto_auth():
+    mastodon = Mastodon(
+        access_token=settings.mastodon_key,
+        api_base_url='https://botsin.space'
+    )
+    return mastodon
+
+
+def twitter_auth():
+    consumer_key = settings.consumer_key
+    consumer_secret = settings.consumer_secret
+    access_token = settings.access_token
+    access_token_secret = settings.access_token_secret
+
+    # App no Twitter
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    bot = tweepy.API(auth)
+    return bot
+
+
+def google_api_auth(arqv_json='credenciais/colaborabot-gAPI.json', subject=None):
+    with open(arqv_json, 'r') as f:
+        conf = json.load(f)
+
+    token_url = conf['token_uri']
+    issuer = conf['client_email']
+    key = conf['private_key']
+    key_id = conf.get('private_key_id')
+
+    header = {'alg': 'RS256'}
+    scopes = [
+        'https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive'
+    ]
+
+    if key_id:
+        header['kid'] = key_id
+
+    # Google puts scope in payload
+    claims = {'scope': ' '.join(scopes)}
+    return AssertionSession(
+        grant_type=AssertionSession.JWT_BEARER_GRANT_TYPE,
+        token_url=token_url,
+        issuer=issuer,
+        audience=token_url,
+        claims=claims,
+        subject=subject,
+        key=key,
+        header=header,
+    )
+
+
+def id_mastodon():
+    id_perfil = settings.mastodon_profile_id
+    return id_perfil
---
+++ credenciais/settings.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('./credenciais') / '.env'
+load_dotenv(dotenv_path=env_path, override=True)
+
+# [Twitter API Keys]
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+
+# [Mastodon API Key]
+mastodon_key = os.environ.get('DONTE_USERCRED')
+
+# [Plataforms IDs]
+mastodon_profile_id = os.environ.get('ID_CONTA_MASTODON')
