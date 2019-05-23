46288e1e1b22b8a99180332a0a392e4f74ed858a
Gabriel Monnerat <contato@quiploy.com>
2019-05-22 22:18:28 -0300

    Utilizar rows em vezes de pandas e numpy
    pandas e numpy são libs sensacionais mas apenas para ler um csv, rows pode
    fazer a mesma coisa de uma maneira mais simples.

--- colaborabot.py
+++ colaborabot.py
@@ -1,7 +1,6 @@
 # Importando as libraries
 
-import numpy as np
-import pandas as pd
+import rows
 import datetime
 import json
 import csv
@@ -17,7 +16,9 @@ from autenticadores import twitter_auth, google_api_auth, masto_auth
 # Parametros de acesso das urls
 
 headers = {
-'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' + \
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
 
 TOTAL_TENTATIVAS = 10
 STATUS_SUCESSO = 200
@@ -123,14 +124,7 @@ def carregar_dados_site():
     Abrindo a lista de portais da transparência e tratando
     informações que serão tratados como NaN para o pandas.
     """
-    df = pd.read_csv(
-        'dados/lista_portais.csv',
-        header=None,
-        names=['url', 'arroba', 'orgao'],
-        sep=';'
-    )
-    df = df.replace(np.nan, '', regex=True)
-    return df
+    return rows.import_from_csv("dados/lista_portais.csv")
 
 
 def busca_disponibilidade_sites(sites):
@@ -139,13 +133,13 @@ def busca_disponibilidade_sites(sites):
     a sua disponibilidade. Caso o código de status
     seja 200 (OK), então ela está disponível para acesso.
     """
-    for index, row in sites.iterrows():
-        url, orgao = row['url'], row['orgao']
+    for row in sites:
+        url, arroba, orgao = row.url, row.arroba, row.orgao
 
         for tentativa in range(TOTAL_TENTATIVAS):
             try:
                 momento = datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
-                resposta = get(row['url'], timeout=30, headers=headers)
+                resposta = get(url, timeout=30, headers=headers)
                 dados = cria_dados(url=url, portal=orgao, resposta=resposta.status_code)
                 preenche_csv(arquivo_logs=arq_log, dados=dados)
                 if resposta.status_code == STATUS_SUCESSO:
